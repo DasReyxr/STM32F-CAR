48b8a893f9d66721b05b239cacb6717196892f05
Roberto Lopez <robertloro12@gmail.com>
2026-06-06 21:13:30 -0600

    Datos del figma agregados

--- JP_Carro/Seguidor_linea/Core/Src/FuzzyLogic.c
+++ JP_Carro/Seguidor_linea/Core/Src/FuzzyLogic.c
@@ -1,5 +1,5 @@
 #include "FuzzyLogic.h"
-
+#include "stm32f4xx_hal.h"
 
 float Trapz(float x, FuzzyArgs args, uint8_t type)
 {
@@ -67,13 +67,10 @@ float Highf(float x) {
 }
 
 
+
 void areas(void){
   /*
   Slopes
-  m_Low  =-1/(L.R-L.L)
-m_Mid1 = 1/(M.C-M.L)
-m_Mid2 =-1/(M.R-M.C)
-m_High = 1/(H.R-H.L)
 Functions of pertinence
 u_Low  = (L.R-x)/(L.R-L.L)
 u_Mid1 = (x-M.L)/(M.C-M.L)
@@ -83,30 +80,45 @@ u_High = (x-HL)/(HR-HL)
   FuzzyArgs Low = {0, 0, 50};
   FuzzyArgs Mid = {25, 50, 75};
   FuzzyArgs High = {50, 0, 100};
-  float x_Low = 30; // Example input for Low
-  float x_mid = 40; // Example input for Mid
-  float x_High = 60; // Example input for High
-  float y_Low = Lowf(x_Low);
-  float y_mid = Mediumf(x_mid);
-  float y_High = Highf(x_High);
+  float x_Low ; // Example input for Low
+  float x_mid ; // Example input for Mid
+  float x_High ; // Example input for High
+  
+  float y_Low;// Lowf(x_Low);
+  float y_mid;//Mediumf(x_mid);
+  float y_High;//Highf(x_High);
   // Low x_l y_l
   
+      // Low and Mid
+    float m_Low = -1/(Low.R-Low.L); 
+    float x_low = (Low.R - y_low)/(m_low);
+
   float C_Low = y_Low*x_Low;
   float T_Low = y_Low*(Low.R-x_Low)/2;
   float A_Low = T_Low+C_Low;
 
   // Medio x_m y_m
-  float TS = (x_mid-x_lm0)*(1-y_mid)/2.0f;
+  float m_Mid0 = 1/(Mid.C-Mid.L);
+  float m_Mid1 =-1/(Mid.R-Mid.C);
+
+  float x_m0 = (y_mid - Mid.L) /m_Mid0;
+  float x_m1 = (y_mid - Mid.R) /m_Mid1;
+
+  float TS = (x_m1-x_m0)*(1-y_mid)/2.0f;
   float TT = (Mid.R-Mid.L)/2.0f;
   float A_M = TT-TS;
 
   float x_Low0 = (y_mid-Mid.L)*(Mid.R-Mid.L);
 
 
-  // Low and Mid
-  float x_lm0 = (y_mid - Mid.L) * (Mid.C - Mid.L); // Derived from m_Mid1 
+  // Mid and High
+
   float Y1 = (Low.R * Mid.C - Low.L * Mid.L)/(Mid.C - Mid.L+ Low.R- Low.L);
   float y_lm = fmin(y_Low,y_Mid);
+  
+  float x_lm0 = (y_lm - Mid.L)/m_Mid0; // Derived from m_Mid1 
+  float x_lm1 = (Low.R - y_lm)/m_low; // Derived from m_Mid1 
+  
   if(y_lm<Y1) 
     float T2 = (x_Low-x_Low0)*(Y1-y_lm)/2.0f;
   else T2 = 0
@@ -114,7 +126,6 @@ u_High = (x-HL)/(HR-HL)
   float TLeft = Y1*(Low.R-Mid.L)/2.0f;
   float A_LowMid = TLeft-T2;
 
-  // Mid and High
   float y_mh0 = (y_High-High.L)*(High.R-High.L);
   float Y2 = (High.R * Mid.R - High.L * Mid.C)/ (Mid.R-Mid.C+High.R-High.L);// (MR-x)/(MR-Mmid)  = (x-HL)/(HR-HL)  
   float y_mh = fmin(y_Low,y_Mid);
