bd304dcc5af68ae63ce909f5ed86380c9428a5b9
Roberto Lopez <robertloro12@gmail.com>
2026-06-07 12:20:22 -0600

    Correciones

--- JP_Carro/Seguidor_linea/Core/Src/FuzzyLogic.c
+++ JP_Carro/Seguidor_linea/Core/Src/FuzzyLogic.c
@@ -91,6 +91,7 @@ float f_High(float y)
 
 
 
+
 void areas(void){
   /*
   Slopes
@@ -107,61 +108,75 @@ u_High = (x-HL)/(HR-HL)
 //  float x_mid ; // Example input for Mid
 //  float x_High ; // Example input for High
   
-  float y_Low;// Lowf(x_Low);
+  float y_low;// Lowf(x_Low);
   float y_mid;//Mediumf(x_mid);
-  float y_High;//Highf(x_High);
+  float y_high;//Highf(x_High);
   // Low x_l y_l
   
   // Low and Mid
-  float m_Low = -1/(Low.R-Low.L); 
+  // Area Low
+  float m_LOW = -1/(Low.R-Low.L); 
   float x_low = f_low(y_low);
 
-  float C_Low = y_Low*x_Low;
-  float T_Low = y_Low*(Low.R-x_Low)/2;
-  float A_Low = T_Low+C_Low;
+  float C_low = y_low*x_low;
+  float T_low = y_low*(Low.R-x_Low)/2;
+  float A_low = T_low+C_low;
+
 
   // Medio x_m y_m
+  //Area Mid (Triangule)
   float m_Mid0 = 1/(Mid.C-Mid.L);
   float m_Mid1 =-1/(Mid.R-Mid.C);
 
-  float x_m0 = f_Mid1(y_mid);
-  float x_m1 = f_Mid2(y_mid);
+  float x_mid0 = f_Mid1(y_mid);
+  float x_mid1 = f_Mid2(y_mid);
 
-  float TS = (x_m1-x_m0)*(1-y_mid)/2.0f;
+  float TS = (x_mid1-x_mid0)*(1-y_mid)/2.0f;
   float TT = (Mid.R-Mid.L)/2.0f;
   float A_M = TT-TS;
 
   float x_Low0 = (y_mid-Mid.L)*(Mid.R-Mid.L);
 
 
-  // Mid and High
-
-  float Y1 = (Low.R * Mid.C - Low.L * Mid.L)/(Mid.C - Mid.L+ Low.R- Low.L);
+  // Mid and low
+  // Area Intersetnction 1
+  float Y1 = (Low.R * Mid.C - Low.L * Mid.L)/(Mid.C - Mid.L+ Low.R- Low.L); // Constante
   float y_lm = fmin(y_Low,y_Mid);
   
-  float x_lm0 = f_Mid1(y_lm); // Derived from m_Mid1 
-  float x_lm1 = f_low(y_lm)(Low.R - y_lm)/m_low; // Derived from m_Mid1 
+  float x_lm0 = f_Mid1(y_lm);  
+  float x_lm1 = f_low(y_lm);
+  float X_1 = f_Mid1(Y1); // Constante 
   
   if(y_lm<Y1) 
-    float T2 = (x_Low-x_Low0)*(Y1-y_lm)/2.0f;
-  else T2 = 0
+    float T1 = (x_lm1-x_lm0)*(Y1-y_lm)/2.0f;
+  else T1 = 0
 
   float TLeft = Y1*(Low.R-Mid.L)/2.0f;
   float A_LowMid = TLeft-T2;
-
+  
+  // mid and high
+  // Area Intersection 2
   float y_mh0 = (y_High-High.L)*(High.R-High.L);
   float Y2 = (High.R * Mid.R - High.L * Mid.C)/ (Mid.R-Mid.C+High.R-High.L);// (MR-x)/(MR-Mmid)  = (x-HL)/(HR-HL)  
   float y_mh = fmin(y_Low,y_Mid);
+  
+  float x_mh0 =  f_High(y_mh);  
+  float x_mh1 = f_Mid2(y_mh); 
+  float X_2 = f_High(Y2);
+  
   if(y_mh<Y1) 
-    float T3 = (x_MidHigh - X_High)*(Y2-y_mh)/2.0f;
-  else T3 = 0;
+    float T2 = (x_mh1 - x_mh0)*(Y2-y_mh)/2.0f;
+  else T2 = 0;
 
   float TRight = (Mid.R-High.L * Y2)/2.0f;
-  float A_MidHigh = TRight-T3;
-    // High x_h y_h
-  float C_High = (High.R-x_High) * y_High;
-  float T_High = (x_High-High.L) * y_High / 2.0f;
-  float A_High = T_High + C_High;
+  float A_MidHigh = TRight-T2;
+  
+  // Area High x_h y_h
+  float m_High = 1/(H.R-H.L);
+  float x_high= f_High(y_high);
+  float C_high = (High.R-x_high) * y_high;
+  float T_high = (x_high-High.L) * y_high / 2.0f;
+  float A_high = T_high + C_high;
 
-  float A_Total = C_Low + T_Low+ TT - TS + C_High+ T2 +T3 - TRight - TLeft;
+  float A_Total = C_low + T_low+ TT - TS + C_high + T_high + T1 +T2 - TRight - TLeft;
   }
